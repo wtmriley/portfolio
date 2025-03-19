8482e23b6c8d6e001bedcd21480a33949389d57b
Riley <111906468+wtmriley@users.noreply.github.com>
2025-03-19 13:54:39 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,21 +12,16 @@ function App() {
       if (scrollingHorizontally) {
         container.scrollBy({
           left: event.deltaY < 0 ? -70 : 70,
+          behavior: "smooth",
         });
         event.preventDefault();
-
-        if (container.scrollLeft >= container.scrollWidth - container.clientWidth) {
-          scrollingHorizontally = false;
-          container.style.overflowY = "auto";
         }
-      }
-    };
-
+      };
     container.addEventListener("wheel", handleScroll, { passive: false });
 
     return () => container.removeEventListener("wheel", handleScroll);
   }, []);
-  
+    
   return (
     <div>
       <h1 className="title">Riley's Portfolio</h1>
