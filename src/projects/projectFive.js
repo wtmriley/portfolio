023e32735d62c59ba232862e5997a604fe9e803f
riley <rshinni1@asu.edu>
2025-05-07 16:30:38 -0600

    graphic design page

--- src/projects/projectFive.js
+++ src/projects/projectFive.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./projectFive.css";
 import { Link } from "react-router-dom";
+import graphic1 from "../images/p5images/Comp-1_2.gif";
+import graphic2 from "../images/p5images/trifold.png";
+import graphic3 from "../images/p5images/trifold2.png";
+import graphic4 from "../images/p5images/magazine.png"
+import graphic5 from "../images/p5images/magazinee2.jpg"
+import graphic6 from "../images/p5images/magazinee3.jpg"
+import graphic7 from "../images/p5images/magazinee4.jpg"
+import graphic8 from "../images/p5images/magazinee5.jpg"
+import graphic9 from "../images/p5images/magazinee6.jpg"
+import graphic10 from "../images/p5images/fold.png"
+import graphic11 from "../images/p5images/myartbaby.gif"
+import graphic12 from "../images/p5images/cityy.png"
+
 
 function ProjectFive() {
 
@@ -14,8 +27,49 @@ function ProjectFive() {
             </div>
 
             <div className="allWork">
-                
+                <div className="amposter">
+                    <div className="amposterTitle">
+                        Animated Arctic Monkeys Concert Poster
+                        </div>
+                    <img src={graphic1} alt="Graphic Design Poster" className="p5img"></img>
+                </div>
+
+                <div className="menu">
+                    <div className="menuTitle">
+                        Trifold Menu
+                    </div>
+                    <img src={graphic2} alt="Trifold Menu" className="p5img"></img>
+                    <img src={graphic3} alt="Trifold Menu" className="p5img"></img>
+                </div>
+
+                <div className="magazine">
+                    <div className="magazineTitle">
+                        Rolling Stone Magazine Recreation
+                    </div>
+                    <div className="magazineall">
+                        <img src={graphic4} alt="Rolling Stone Magazine Cover" className="p5img1"></img>
+                        <img src={graphic5} alt="Rolling Stone Magazine Page" className="p5img2"></img>
+                        <img src={graphic6} alt="Rolling Stone Magazine Page" className="p5img3"></img>
+                        <img src={graphic7} alt="Rolling Stone Magazine Page" className="p5img4"></img>
+                        <img src={graphic8} alt="Rolling Stone Magazine Page" className="p5img5"></img>
+                        <img src={graphic9} alt="Rolling Stone Magazine Back Cover" className="p5img6"></img>
+                    </div>
+                </div>
+
+                <div className="fold">
+                    <div className="foldTitle">
+                        Photoshop Art
+                    </div>
+                    <div className="graphicall">
+                        <img src={graphic10} alt="Photoshop Art" className="p5img7"></img>
+                        <img src={graphic11} alt="Photoshop Art" className="p5img8"></img>
+                        <img src={graphic12} alt="Photoshop Art" className="p5img9"></img>
+                    </div>
+                </div>
+
             </div>
+
+
             <div className="footer">
                 <div className="footergroup">
                 <Link to="/project/2" className="footerText">Project One</Link>
