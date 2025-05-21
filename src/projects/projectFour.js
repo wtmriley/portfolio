820291425e5315932a45d01d94e7d017426ae1c2
riley <rshinni1@asu.edu>
2025-05-21 11:40:54 -0600

    added game vid

--- src/projects/projectFour.js
+++ src/projects/projectFour.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./projectFour.css";
 import { Link } from "react-router-dom";
+import gamevid from "../images/p4images/game.mp4"
 
 function ProjectFour() {
 
@@ -14,15 +15,21 @@ function ProjectFour() {
                 iPhone Game App
             </div>
 
-            <div className="p4vid">
-                
-            </div>
+            <div className="gamegrid">
+                <div className="p4vid">
+                    <video autoPlay muted loop playsInline className="vide">
+                        <source src={gamevid} type="video/mp4"></source>
+                    </video>
+                </div>
 
-
-            <div className="p4sum">
-                <div className="p4summary">
-                    &nbsp; &nbsp; &nbsp;
-                    the reqirement for this project was to create a game app for ios devices.  The language I used was Swift, and I wrote the app using xCode.
+                <div className="p4sum">
+                    <div className="p4summary">
+                        &nbsp; &nbsp; &nbsp;
+                        the reqirement for this project was to create a game app for ios devices. 
+                        The language I used was Swift, and I wrote the app using xCode.
+                        Using gravity, users rotate their phone to move the blocks.
+                        The goal is for all the blocks to be the same color. 
+                    </div>
                 </div>
             </div>
 
