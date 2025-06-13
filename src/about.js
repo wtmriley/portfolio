bb6663b8bcd047d85d2ac1ae57c4e05ad63618fc
riley <rshinni1@asu.edu>
2025-06-13 12:23:41 -0600

    about edits'

--- src/about.js
+++ src/about.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './about.css';
 import { Link } from "react-router-dom";
+import linkedin from "./images/linkedin icon.png";
+import github from "./images/github icon.png";
 
 function About() {
     /* put little arrow here for nav */
@@ -26,32 +28,33 @@ function About() {
                 </div>
 
                 <div className="aboutsummary">
-                    <div className="aboutst">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; hi :) </div>
-                    <br /> I'm <strong>Riley Shinnick</strong>, a creative designer and developer based in Denver, Colorado.
-                    I've always been drawn to the way design shapes people's experiences. That's what drives my work—creating <strong>intuitive, accessible, and visually engaging solutions</strong> that make <strong>technology feel seamless for everyone. </strong> 
-                    I specialize in building responsive, user-centered web apps and have a knack for making them visually appealing. 
-                    <br /> <br />When I'm not coding or designing, I'm likely reading, playing Stardew Valley, or doing something outdoors. 
-                    <br /> <br />I'm always looking for new projects that challenge me to think differently--whether that's in web development, UX design, or somewhere in that realm.
-                    <br /> <br />I'm looking forward to hearing from you!
+                    <div className="aboutst">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; hi, I'm Riley :) </div>
+                    <br /> <br />I’m a designer and developer who values thoughtful design, strong communication, and ease of use.
+                    <br /> <br />I come from a background in UX and media design, and I love building things that are clear, useful, and engaging. I especially enjoy working on projects that involve creative freedom and problem solving.
+                    <br /> <br />I’m looking for opportunities where I can contribute visually and technically, learn from others, and grow as a developer.
+                    <br /> <br />When I’m not coding or designing, I’m probably
+                    <a href="https://open.spotify.com/user/wtmrileyu?si=6868c047755f4f07" className="music" target="_blank" aria-label="Visit my Spotify profile">listening to music,</a> 
+                    <a href="https://app.thestorygraph.com/profile/wtmriley" className="read" target="_blank" aria-label="Visit my Storygraph profile">reading</a>    
+                     or playing Stardew Valley. 
+
+                    <br /> <br />
+                    
                 </div>
 
                 <div className="about-details">  
                     <div className="aboutLinks">
                         <a href="https://github.com/wtmriley" className="github" target="_blank" aria-label="Visit my GitHub profile">
-                        github
+                        <img src = {github} className="githubicon" alt="github icon"></img>
+                        check out my projects
                         </a>
                         
-                        <a href="www.linkedin.com/in/rileyshinnick" className="linkedin" target="_blank" aria-label="Visit my LinkedIn profile">
-                        linkedin
+                        
+
+                        <a href="https://www.linkedin.com/in/rileyshinnick/" className="linkedin" target="_blank" aria-label="Visit my LinkedIn profile">
+                        <img src = {linkedin} className="linkedinicon" alt="linkedin icon"></img>
+                        connect with me!
                         </a>
                         
-                        <a href="https://app.thestorygraph.com/profile/wtmriley" className="storygraph" target="_blank" aria-label="Visit my Storygraph profile">
-                        storygraph
-                        </a>
-                        
-                        <a href="https://open.spotify.com/user/wtmrileyu?si=6868c047755f4f07" className="spotify" target="_blank" aria-label="Visit my Spotify profile">
-                        spotify
-                        </a>
 
                     </div>
                 </div>
