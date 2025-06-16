f1d0f0e047a8c40ea2e483c76c9b8742958404fd
riley <rshinni1@asu.edu>
2025-06-16 10:40:21 -0600

    about styling

--- src/about.js
+++ src/about.js
@@ -32,14 +32,15 @@ function About() {
                     <br /> <br />I’m a designer and developer who values thoughtful design, strong communication, and ease of use.
                     <br /> <br />I come from a background in UX and media design, and I love building things that are clear, useful, and engaging. I especially enjoy working on projects that involve creative freedom and problem solving.
                     <br /> <br />I’m looking for opportunities where I can contribute visually and technically, learn from others, and grow as a developer.
-                    <br /> <br />When I’m not coding or designing, I’m probably
-                    <a href="https://open.spotify.com/user/wtmrileyu?si=6868c047755f4f07" className="music" target="_blank" aria-label="Visit my Spotify profile">listening to music,</a> 
-                    <a href="https://app.thestorygraph.com/profile/wtmriley" className="read" target="_blank" aria-label="Visit my Storygraph profile">reading</a>    
-                     or playing Stardew Valley. 
-
+                    <br /> <br />When I’m not coding or designing, I’m probably{" "}
+                    <a href="https://open.spotify.com/user/wtmrileyu?si=6868c047755f4f07" className="music" target="_blank" aria-label="Visit my Spotify profile">listening to music,</a>{" "} 
+                    <a href="https://app.thestorygraph.com/profile/wtmriley" className="read" target="_blank" aria-label="Visit my Storygraph profile">reading</a>{" "}
+                    or playing Stardew Valley. 
                     <br /> <br />
-                    
+                    <hr className="hr"/>
                 </div>
+
+                
 
                 <div className="about-details">  
                     <div className="aboutLinks">
@@ -54,7 +55,7 @@ function About() {
                         <img src = {linkedin} className="linkedinicon" alt="linkedin icon"></img>
                         connect with me!
                         </a>
-                        
+
 
                     </div>
                 </div>
